ff4af75bc9bc012893a46d14116fcf1bbbf01191
Max <maxonline@mail.ru>
2023-12-18 05:08:05 +0300

    feat(VAutocomplete): add highlighting

--- src/components/VAutocomplete.tsx
+++ src/components/VAutocomplete.tsx
@@ -30,10 +30,73 @@ const VirtualizedListboxComponent = forwardRef<
   );
 });
 
+function CommentBox({
+  data,
+  highlight,
+}: {
+  data: Comment;
+  highlight?: string;
+}) {
+  const re = highlight && new RegExp(`(${highlight})`, "gi");
+  return (
+    <Box
+      component="ul"
+      sx={{
+        listStyle: "none",
+        paddingInlineStart: 1,
+        marginBlock: 0.5,
+        width: "99%",
+      }}
+      bgcolor="lightgray"
+      borderRadius={1}
+    >
+      {["name", "email", "body"].map((key) => {
+        const value = data[key as keyof Comment];
+        const parts =
+          typeof value === "string" && re ? value.split(re) : [value];
+
+        return (
+          <Box
+            component="li"
+            key={key}
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+            overflow="hidden"
+          >
+            <Typography
+              component="span"
+              variant="caption"
+              color="green"
+              fontWeight={500}
+            >
+              {key}:&nbsp;
+            </Typography>
+            {parts.map((part, key) => (
+              <Typography
+                key={key}
+                component="span"
+                color="tomato"
+                fontWeight={
+                  typeof part === "string" && re && re.test(part)
+                    ? 700
+                    : undefined
+                }
+              >
+                {part}
+              </Typography>
+            ))}
+          </Box>
+        );
+      })}
+    </Box>
+  );
+}
+
 export default function VAutocomplete() {
   const id = useId();
   const [trigger, { data = [], isLoading }] = useLazyGetPostsQuery();
   const search = useDebounce(trigger);
+
   return (
     <Autocomplete
       id={id}
@@ -41,58 +104,23 @@ export default function VAutocomplete() {
       onOpen={() => !data.length && trigger()}
       sx={{ width: 700 }}
       onInputChange={(_, value, reason) => {
-        switch (reason) {
-          case "input":
-            return search(value);
-          default:
-            trigger();
-        }
+        reason === "input" ? search(value) : trigger();
       }}
       loading={isLoading}
-      renderInput={(params) => <TextField {...params} label="Comments" />}
+      renderInput={(params) => (
+        <TextField {...params} label="Type to comments searching" />
+      )}
       getOptionLabel={({ name }) => name}
       filterOptions={(options) => options}
       ListboxComponent={VirtualizedListboxComponent}
-      renderOption={(props, option) => (
+      renderOption={(props, option, { inputValue }) => (
         <Box
           component="li"
           {...props}
           key={option.id}
           sx={{ "&.MuiAutocomplete-option.MuiBox-root": { paddingY: 0 } }}
         >
-          <Box
-            component="ul"
-            sx={{
-              listStyle: "none",
-              paddingInlineStart: 1,
-              marginBlock: 0.5,
-              width: "99%",
-            }}
-            bgcolor="lightgray"
-            borderRadius={1}
-          >
-            {["name", "email", "body"].map((key) => (
-              <Box
-                component="li"
-                key={key}
-                whiteSpace="nowrap"
-                textOverflow="ellipsis"
-                overflow="hidden"
-              >
-                <Typography
-                  component="span"
-                  variant="caption"
-                  color="green"
-                  fontWeight={500}
-                >
-                  {key}:&nbsp;
-                </Typography>
-                <Typography component="span" color="tomato">
-                  {option[key as keyof Comment]}
-                </Typography>
-              </Box>
-            ))}
-          </Box>
+          <CommentBox data={option} highlight={inputValue} />
         </Box>
       )}
     />
